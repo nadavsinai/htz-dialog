3485be7609a32239571325710b6a4a725022e8cd
TxHawks <tofu.hawks@gmail.com>
2016-08-04 12:35:43 +0300

    Fix type in dependency name

--- jspm.config.js
+++ jspm.config.js
@@ -24,7 +24,6 @@ SystemJS.config({
   map: {
     "htz-dispatch-event": "github:haaretz/htz-dispatch-event@1.0.1",
     "htz-get-focusables": "github:haaretz/htz-get-focusables@1.0.2",
-    "htz-getfocusables": "github:haaretz/htz-get-focusables@1.0.2",
     "plugin-babel": "npm:systemjs-plugin-babel@0.0.13"
   },
   packages: {}
