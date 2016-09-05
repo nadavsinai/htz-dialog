64a0b5fc98b3ab546ba943455227f400edf17044
TxHawks <tofu.hawks@gmail.com>
2016-09-05 18:02:34 +0300

    Store all initialized instances in an array

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@
 import dispatchEvent from 'htz-dispatch-event';
 import getFocusables from 'htz-get-focusables';
 
+/**
+ * An array of all initialized instances
+ *
+ * @type {Array}
+ *
+ * @private
+ */
+const allInstances = [];
 
 /**
  * Initialize a dialog.
@@ -335,18 +343,24 @@ export default function htzDialog(
   // --- Return Public API --- //
   /**
    * A public API for programmatically controlling the initialized dialog instance.
-   * @typedef {Object} module:htz-dialog~API
+   * @typedef {Object} module:htz-dialog#API
+   * @prop {HTMLElement} wrapper - The wrapper element of the current dialog
    * @prop {Boolean} isVisible - Indicates if the instance is currently visible.
    * @prop {module:htz-dialog~show} show - Reveal instance.
    * @prop {module:htz-dialog~hide} hide - Hide instance.
    * @prop {module:htz-dialog~next} next - Move to next dialog in wrapper, if one exists
    * @prop {module:htz-dialog~prev} prev - Move to previous dialog in wrapper, if one exists
    */
-  return {
+  const api = {
+    wrapper,
     isVisible,
     hide,
     show,
     next,
     prev,
   };
+
+  allInstances.push(api);
+
+  return api;
 }
