7c0f67f0f6600ee42ba8b228b416182b47a20a80
nadavsinai <ns@nadavsinai.com>
2017-01-08 13:27:55 +0200

    add  1st argument test case

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,5 +3,11 @@ import  htzDialog from './index';
 describe('test initiation', () => {
   it('is a function', () => {
     expect(htzDialog).to.be.a('function');
-  })
+  });
+
+  it('throws if first argument is not instance of HTMLElemnt', function () {
+    expect(() => {
+      htzDialog();
+    }).to.throw();
+  });
 });
