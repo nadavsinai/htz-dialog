83ab53f7737de62b038aa061c1b131e634ffdce5
nadavsinai <ns@nadavsinai.com>
2017-01-08 13:32:52 +0200

    show code in test is just code... failing test - we found a bug

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,8 +6,15 @@ describe('test initiation', () => {
   });
 
   it('throws if first argument is not instance of HTMLElemnt', function () {
-    expect(() => {
-      htzDialog();
-    }).to.throw('the argument provided as the `wrapper` parameter must be an HTMLElement');
+
+    let tries = [undefined,[],{},'a',1, new Map()];
+    for (let test of tries) {
+      expect(() => {
+        htzDialog(test);
+      }).to.throw('the argument provided as the `wrapper` parameter must be an HTMLElement');
+    }
   });
+
+
+
 });
