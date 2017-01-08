e54652326acabaf4e1d926411f3a4346dbc93fe7
nadavsinai <ns@nadavsinai.com>
2017-01-08 23:20:28 +0200

    add some test spec stubs and first event listeners test demo

--- src/index.spec.js
+++ src/index.spec.js
@@ -32,5 +32,25 @@ describe('test initiation', () => {
     expect(wrapper.parentElement).to.equal(appendTo);
   });
 
+  it('moves wrapper if attribute data-htz-dialog-append-to exists and its value is found on document via getElementById')
+  it('returns public api')
 
 });
+describe('it adds event listeners', () => {
+  it('adds show/hide event listeners to buttons found via attributes', () => {
+    let wrapper = document.createElement('div');
+    wrapper.id = 'test';
+    let button = document.createElement('button');
+    button.setAttribute('data-htz-dialog-show', 'test');
+    button.addEventListener = sinon.spy();
+    wrapper.appendChild(button);
+    document.body.appendChild(wrapper);
+    let api = htzDialog(wrapper);
+    expect(button.addEventListener).to.have.been.calledWith('click', api.show)
+  });
+  it('adds next/prev event listeners to buttons found via attributes');
+
+  it('adds onKeyDown listener to the wrapper for keyCode 27 if element is visible');
+});
+
+
