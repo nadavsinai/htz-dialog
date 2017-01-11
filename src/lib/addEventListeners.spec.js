41032f9e08f869d31cda4aef0a41bb14d22823fa
nadavsinai <ns@nadavsinai.com>
2017-01-11 20:59:53 +0200

    fix add event listeners spec not to cross the boundry of internal implmendation

--- src/lib/addEventListeners.spec.js
+++ src/lib/addEventListeners.spec.js
@@ -1,15 +1,16 @@
-import  htzDialog from '../index';
+import  addEventListners from './addEventListners';
 describe('it adds event listeners', () => {
   it('adds show/hide event listeners to buttons found via attributes', () => {
     let wrapper = document.createElement('div');
     wrapper.id = 'test';
     let button = document.createElement('button');
     button.setAttribute('data-htz-dialog-show', 'test');
-    button.addEventListener = sinon.spy();
     wrapper.appendChild(button);
     document.body.appendChild(wrapper);
-    let api = htzDialog(wrapper);
-    expect(button.addEventListener).to.have.been.calledWith('click', api.show)
+    let dialogState = { wrapper, show: sinon.spy()}
+    addEventListners({ wrapper });
+    button.click();
+    expect(dialogState.show).to.have.been.called();
   });
   it('adds next/prev event listeners to buttons found via attributes');
 
