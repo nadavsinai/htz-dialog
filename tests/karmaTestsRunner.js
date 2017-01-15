2e3da721e1268ad1b27f841e2a2d1a5dcb0a9cd0
nadavsinai <ns@nadavsinai.com>
2017-01-15 11:24:35 +0200

    for parity - remove karma's mock jsdom

--- tests/karmaTestsRunner.js
+++ tests/karmaTestsRunner.js
@@ -1,8 +1,5 @@
 import chaiAsPromised from 'chai-as-promised';
 import dirtyChai from 'dirty-chai';
-window.jsdom = function () {
-  return Function.prototype;
-};
 
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
